7813ffa3348f15308c4dc6038b5a896244f4ab99
qza36 <an9qi@yandex.com>
2024-03-07 18:15:26 +0800

    创建发布者

--- example_topic_rclcpp/src/topic_publisher_01.cpp
+++ example_topic_rclcpp/src/topic_publisher_01.cpp
@@ -1,4 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp" //导入消息接口
 
 class TopicPublisher01 : public rclcpp::Node
 {
@@ -7,10 +8,24 @@ public:
     TopicPublisher01(std::string name) : Node(name)
     {
         RCLCPP_INFO(this->get_logger(), "%s节点已经启动.", name.c_str());
+        //创建发布者
+        command_publisher_ = this->create_publisher<std_msgs::msg::String>("command", 10);
     }
 
 private:
-    // 声明节点
+    void timer_callback(){
+        // 创建一个消息
+        std_msgs::msg::String message;
+        message.data =  "Hello World!";
+        //日志打印
+        RCLCPP_INFO(this->get_logger(), "发布消息: '%s'", message.data.c_str());
+        //发布消息
+        command_publisher_->publish(message);
+    }
+    //声明定时器指针
+    rclcpp::TimerBase::SharedPtr timer_;
+    // 声明话题发布者
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr command_publisher_;
 };
 
 int main(int argc, char **argv)
