bbd21658194b8a484328dc2e2f09786bdd048430
qza36 <an9qi@yandex.com>
2024-03-13 16:30:59 +0800

    完成动作通信代码

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## 2024/3/12 写参数,学动作
 - 参数代码通过，但是执行<br><code>ros2 run example_parameters_rclcpp parameters_basic --ros-args -p rcl_log_level:=50</code><br>没有预期效果
 ## 2024/3/13 写动作代码
+- 代码写完，编译失败
+```
+    Could not find a package configuration file provided by
+  "robot_control_interfaces" with any of the following names:
+
+    robot_control_interfacesConfig.cmake
+    robot_control_interfaces-config.cmake
+
+```
--- example_action_rclcpp/include/example_action_rclcpp/robot.h
+++ example_action_rclcpp/include/example_action_rclcpp/robot.h
@@ -8,9 +8,23 @@ copyright
 
 class Robot {
  public:
-  Robot() = default;
-  ~Robot() = default;
+    using MoveRobot = robot_control_interfaces::action::MoveRobor;
+    Robot() = default;
+    ~Robot() = default;
+    float move_step();//移动机器人
+    bool set_goal(float goal);//移动一段距离
+    float get_current_pose();//获得当前位置
+    int get_status();//获得机器人状态
+    bool close_goal();//是否接近目标
+    void stop_move();//停止移动
+
+
  private:
+    float current_pose_ = 0.0;//声明当前位置
+    float target_pose_ = 0.0;//声明目标位置
+    float move_distance = 0.0;//声明移动距离
+    std::atomic<bool>= cancel_flag_{false};//取消标志
+    int status_ = MoveRobot::Feedback::STATUS_STOP;//声明机器人状态
 };
 
 #endif  // EXAMPLE_ACTIONI_RCLCPP_ROBOT_H_
